31ddcb76eebe223c23aee7e87ddb97dda2720ddf
Marco <mr.lepore@hotmail.it>
2021-03-10 15:21:54 +0100

    primo punto della consegna eseguito

---
+++ js/main.js
@@ -0,0 +1,50 @@
+/* - Creare un oggetto che descriva uno studente con le
+seguenti proprietà: nome, cognome e età.
+Stampare a schermo attraverso il for in tutte le proprietà. */
+
+
+var studente = {
+    "nome" : "Mario",
+    "cognome" : "Bianchi",
+    "eta" : 20
+}
+
+for (var chiave in studente) {
+    alert(studente[chiave]);
+}
+
+/* Creare un array di oggetti di studenti. Ciclare su tutti gli
+studenti e stampare per ognuno nome e cognome */
+
+var studenti = [
+    {
+        "nome" : "Marco",
+        "cognome" : "Lepore",
+        "eta" : "32"
+    }
+    {
+        "nome" : "Matteo",
+        "cognome" : "Rossi",
+        "eta" : "30"
+    }
+    {
+        "nome" : "Mario",
+        "cognome" : "Bianchi",
+        "eta" : "31"
+    }
+    {
+        "nome" : "Giuseppe",
+        "cognome" : "Verdi",
+        "eta" : "20"
+    }
+    {
+        "nome" : "Michele",
+        "cognome" : "Gialli",
+        "eta" : "33"
+    }
+    {
+        "nome" : "Fabio",
+        "cognome" : "Forghieri",
+        "eta" : "31"
+    }
+]
